6921fe5b6d641fb3a64ca3faee944f15f9aef8af
Ricardo Leon <josericardobl1@outlook.com>
2022-12-28 16:05:57 -0500

    Se modifica las versiones del poyecto para agregar Eureka como Client

--- src/main/java/com/microservicios/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/microservicios/springboot/app/item/SpringbootServicioItemApplication.java
@@ -1,12 +1,17 @@
 package com.microservicios.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
-
-@SpringBootApplication
+//@ImportAutoConfiguration({FeignAutoConfiguration.class})
 @EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
+
 public class SpringbootServicioItemApplication {
 
 	public static void main(String[] args) {
