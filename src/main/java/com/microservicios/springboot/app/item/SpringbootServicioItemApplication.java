9219cee8ebb0806e8b846cc8510dd466345ed572
Ricardo Leon <josericardobl1@outlook.com>
2023-01-11 13:27:11 -0500

    Se cambia la version del proyecto y se implemneta Hystrix

--- src/main/java/com/microservicios/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/microservicios/springboot/app/item/SpringbootServicioItemApplication.java
@@ -3,11 +3,13 @@ package com.microservicios.springboot.app.item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 //@ImportAutoConfiguration({FeignAutoConfiguration.class})
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
--- src/main/java/com/microservicios/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/microservicios/springboot/app/item/controllers/ItemController.java
@@ -1,7 +1,9 @@
 package com.microservicios.springboot.app.item.controllers;
 
 import com.microservicios.springboot.app.item.models.Item;
+import com.microservicios.springboot.app.item.models.Producto;
 import com.microservicios.springboot.app.item.models.service.ITemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +15,27 @@ public class ItemController {
     @Autowired
     @Qualifier("ItemServiceFeign")
     private ITemService iTemService;
+
     @RequestMapping(value = "/mostrar", method = RequestMethod.GET)
     // @GetMapping("/mostrar")
-    public List<Item> Listar(){
+    public List<Item> Listar() {
         return iTemService.findAll();
     }
 
+    @HystrixCommand(fallbackMethod = "MetodoAlternativo")
     @GetMapping("/buscar/{id}/cantidad/{cantidad}")
-    public Item Detalle(@PathVariable Long id, @PathVariable Integer cantidad){
-        return   iTemService.findById(id,cantidad);
+    public Item Detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+        return iTemService.findById(id, cantidad);
+    }
+
+    public Item MetodoAlternativo(Long id, Integer cantidad) {
+        Item item = new Item();
+        Producto producto = new Producto();
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Producto de emergencia...");
+        producto.setPrecio(0.0);
+        item.setProducto(producto);
+        return item;
     }
 }
